8b1ed157748edfdbb9163ee9203d2de57ef9901a
orionjoss <orionjoss@gmail.com>
2023-08-17 13:07:10 +0100

    validates

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+  has_many :bookmarks
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: true, presence: true
   validates :rating, inclusion: { in: [1,2,3,4,5], allow_nil: true }
   validates :overview, presence: true
-  has_many :boomarks
+  has_many :bookmarks
 end
