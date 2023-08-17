d695cd24d00dd1d739a90d3ffb121e098c5bef82
orionjoss <orionjoss@gmail.com>
2023-08-17 17:21:28 +0100

    create list done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,14 +14,16 @@ class ListsController < ApplicationController
 
   def create
     # make an instance of list
-
+  # save the instance of the list in the db
+   # redirect the user to the index page
     @list = List.new(list_params)
-    # save the instance of the list in the db
-
-
-    # redirect the user to the index page
+    if @list.save
+      redirect_to @list, notice: 'List created with success.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-
+  
   private
 
   def list_params
