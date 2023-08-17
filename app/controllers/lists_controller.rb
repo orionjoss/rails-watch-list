33394539a4f57130f02733003e395176ac0b677b
orionjoss <orionjoss@gmail.com>
2023-08-17 17:17:48 +0100

    create action done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,4 +11,21 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+
+  def create
+    # make an instance of list
+
+    @list = List.new(list_params)
+    # save the instance of the list in the db
+
+
+    # redirect the user to the index page
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-resources :lists, only: [:index, :new, :show]
+resources :lists, only: [:index, :new, :show, :create]
 end
